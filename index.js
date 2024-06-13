080e6a44dbda92301557784412264659128ce63d
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-06-13 08:44:21 +0530

    hello world

---
+++ index.js
@@ -0,0 +1,21 @@
+// import express from "express";
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("soamtripathy");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page</h1>");
+});
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai aur Code</h2>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
